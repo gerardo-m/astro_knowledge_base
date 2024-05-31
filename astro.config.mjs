0b537cce821b46746347737729bd6e579d6a147f
Gerardo Miranda <https://GerardoMiranda@git.assembla.com>
2024-05-31 15:40:30 -0400

    Added dependencies

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(),pagefind()]
+});
